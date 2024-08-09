5513c7a635fca9ba4e3bb1c3776f398011ce586f
nagendrakumarmutta <mnagendra@iyevasoft.com>
2024-08-09 13:19:41 +0530

    Armstrong practice Java logic

--- Hello.java
+++ Hello.java
@@ -1,30 +1,36 @@
 import java.util.Scanner;
 
-class BinarySearch{
+class ArmStrong{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int first, last, middle, search;
-        int arr[] = new int[n];
-       
-        for(int i=0; i<n; i++){
-            arr[i] = in.nextInt();
+        int temp=n;
+        int digits=0;
+        while (temp!=0){
+            digits++;
+            temp/=10;
         }
-         search = in.nextInt();
-        first = 0;
-        last = n-1;
-        middle = (first + last)/2;
 
-        while (first <= last) {
-            if(arr[middle] < search){
-                first = middle+1;
-            }else if(arr[middle] == search) {
-                System.out.println(search + " found at location "+ (middle+1));
-                break;
-            }else{ 
-                last = middle-1;
-            }
-            middle = (first+last)/2;
+        temp = n;
+        int remainder=0; 
+        int sum=0;
+        while (temp!=0) {
+             remainder = temp%10;
+             sum += power(remainder, digits);
+             temp/=10;
+        }
+        if(sum == n){
+            System.out.println(n + " is an Armstrong number");
+        }else{
+            System.out.println(n + " is not an Armstrong number");
         }
     }
+
+    public static int power(int r, int d){
+            int c=0; int p=1;
+            for(c=0; c<d; c++){
+                p *= r;
+            }
+            return p;
+        }
 }
