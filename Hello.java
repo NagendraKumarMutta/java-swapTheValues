57ccd2373ab52363e6d89d017fd0bd25a4264380
nagendrakumarmutta <mnagendra@iyevasoft.com>
2024-08-09 14:35:11 +0530

    Binary search

--- Hello.java
+++ Hello.java
@@ -1,36 +1,28 @@
 import java.util.Scanner;
 
-class ArmStrong{
+class BinarySearch{
     public static void main(String args[]){
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int temp=n;
-        int digits=0;
-        while (temp!=0){
-            digits++;
-            temp/=10;
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = in.nextInt();
         }
+        int search = in.nextInt();
+        int first = 0;
+        int last = n-1;
+        int middle=(first+last)/2;
 
-        temp = n;
-        int remainder=0; 
-        int sum=0;
-        while (temp!=0) {
-             remainder = temp%10;
-             sum += power(remainder, digits);
-             temp/=10;
-        }
-        if(sum == n){
-            System.out.println(n + " is an Armstrong number");
-        }else{
-            System.out.println(n + " is not an Armstrong number");
+        while (first<=last) {
+            if(arr[middle] < search){
+                first = middle+1;
+            }else if(arr[middle] == search){
+                System.out.println(search + " found at location "+ (middle+1));
+                break;
+            }else{
+                last = middle-1;
+            }
+            middle = (first + last)/2;
         }
     }
-
-    public static int power(int r, int d){
-            int c=0; int p=1;
-            for(c=0; c<d; c++){
-                p *= r;
-            }
-            return p;
-        }
-}
+    }
