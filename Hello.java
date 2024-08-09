99ec3311a55cd7923b39e1f44eee67011d426b6e
nagendrakumarmutta <mnagendra@iyevasoft.com>
2024-08-09 17:24:09 +0530

    F to C

--- Hello.java
+++ Hello.java
@@ -1,30 +1,11 @@
 import java.util.Scanner;
 
-class BubbleSort{
+class FahernheitToCelcius{
     public static void main(String args[]){
-           Scanner in = new Scanner(System.in);
-           System.out.println("How many number you want to sort with BubbleSort Algorithm");
-           int n = in.nextInt();
-           int arr[] = new int[n];
-           int swap = 0;
-
-           System.out.println("Enter "+n+" elements to sort");
-           for(int i=0; i<n; i++){
-             arr[i] = in.nextInt();
-           }
-
-           for(int i=0; i<(n-1);i++ ){
-            for(int j=0; j<(n-i-1); j++){
-                if(arr[j]>arr[j+1]){
-                   swap = arr[j];
-                   arr[j] = arr[j+1];
-                   arr[j+1] = swap;
-                }
-            }
-           }
-           System.out.println("Sorting elements are: ");
-           for(int i=0; i<n; i++){
-            System.out.println(arr[i]);
-           }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter temperature in Fahrenheit");
+        int Temperature = in.nextInt();
+        int celcius = ((Temperature - 32)*5)/9;
+        System.out.println("Temperature in celcius is: "+celcius+"c");
     }
 }
